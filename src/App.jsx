b941a361378f068b1f722fd629e34d5c9ff37761
MarlonPelau <MarlonRegis@pursuit.org>
2024-03-01 15:48:33 -0500

    adding code for functionality in components and app files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { useState } from 'react';
+
+// 3 components here
+import Transactions from "./Transactions";
+import TransactionDetails from "./TransactionDetails";
+import TransactionForm from "./TransactionForm";
 
 const App = () => {
-  return <div>Budgeting App</div>
+  const [transactions, setTransactions] = useState([]);
+  // this state purpose to toggle the TransactionDetails component and send the id
+  const [toggleDetails, setToggleDetails] = useState({show: false, id: null});
+  
+  return <div>Budgtr</div>
 }
 
 export default App
---
+++ src/Transactions.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+
+const Transactions = ({transactions, setTransactions, setToggleDetails}) => {
+    
+  if(transactions.length === 0) return null;
+
+  function handleDelete(id) {
+    const options = {
+      method: "DELETE",
+    };
+
+        fetch(`http://localhost:3300/api/transactions/${id}`, options)
+            .then((res) => res.json())
+            .then((data) => setTransactions(data.transactions));
+    }
+
+return <div>
+        <h1>Transactions</h1>
+        {transactions.map(({id, item_name, amount, category}) =>
+        <div key={id}>
+            <h2>Item Name: {item_name}</h2>
+            <h3>Amount: {amount}</h3>
+            <Link to={`/${id}`}>
+                <button>Details</button>
+            </Link>
+            <button onClick={() => setToggleDetails({show: true, id})} >Details</button>
+            <button onClick={() => handleDelete(id)}>Delete</button>
+            <hr />
+        </div>
+        )}
+        </div>;
+};
+
+export default Transactions;
